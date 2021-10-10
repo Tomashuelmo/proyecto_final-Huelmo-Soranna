996fecf1395edb487fa6589be05667e499bed627
MaximoSoranna <81201351+MaximoSoranna@users.noreply.github.com>
2021-10-09 21:11:23 -0300

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,13 @@
+function ir(){
+ var c=1234;
+ var u="ADMIN";
+ if(document.form.password.value==c && document.form.login.value==u)
+ window.location="index.html";
+
+ else
+ {
+ alert("PorFavor ingrese Usuario y Contraseña correctos.");
+ }
+
+
+} 
